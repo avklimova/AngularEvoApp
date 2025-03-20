84828b61c30aca932c108366452a1b8c134feee9
avklimova <sav_rhome@mail.ru>
2025-03-20 13:29:18 +0300

    SecondApp

--- SecondEvoApp/src/app/app.module.ts
+++ SecondEvoApp/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SecondComponentComponent } from './second-component/second-component.component';
+import { CounterComponent } from './counter/counter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SecondComponentComponent
+    SecondComponentComponent,
+    CounterComponent
   ],
   imports: [
     BrowserModule,
